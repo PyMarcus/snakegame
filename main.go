fb99da6dd4592f7239a8caad11557b5ea02bada0
PyMarcus <marcus-v@outlook.com>
2025-04-16 15:56:23 -0300

    feat: created game over screen

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
 
 	"github.com/PyMarcus/snakegame/snake"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+
 )
 
 func main() {
+	s, _ := text.NewGoTextFaceSource(bytes.NewReader(fonts.MPlus1pRegular_ttf))
+	snake.MplusFaceSource = s
+
 	direction := snake.Point{}
-	
+
 	g := &snake.Game{Snake: snake.StartSnakePosition(), Direction: direction}
 	g.SpawnFood()
 
--- snake/game.go
+++ snake/game.go
@@ -6,20 +6,26 @@ import (
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Game struct {
-	Snake []Point
-	Direction Point
-	food Point
+	Snake      []Point
+	Direction  Point
+	food       Point
 	lastUpdate time.Time
+	GameOver   bool
 }
 
 func (g *Game) Update() error {
+	if g.GameOver {
+		return nil
+	}
+
 	g.keyboardListenerBind()
 
-	if time.Since(g.lastUpdate) < time.Duration(GAME_SPEED){
+	if time.Since(g.lastUpdate) < time.Duration(GAME_SPEED) {
 		return nil
 	}
 
@@ -41,73 +47,82 @@ func (g Game) drawSnakePoints(screen *ebiten.Image) {
 	for _, point := range g.Snake {
 		vector.DrawFilledRect(
 			screen,
-			float32(point.x * RECT_SIZE),
-			float32(point.y * RECT_SIZE),
+			float32(point.x*RECT_SIZE),
+			float32(point.y*RECT_SIZE),
 			float32(RECT_SIZE),
 			float32(RECT_SIZE),
-			color.RGBA{0,255,0,55},
+			color.RGBA{0, 255, 0, 55},
 			true,
 		)
 	}
 
 	vector.DrawFilledRect(
 		screen,
-		float32(g.food.x * RECT_SIZE),
-		float32(g.food.y * RECT_SIZE),
+		float32(g.food.x*RECT_SIZE),
+		float32(g.food.y*RECT_SIZE),
 		float32(RECT_SIZE),
 		float32(RECT_SIZE),
-		color.RGBA{255,0,0,55},
+		color.RGBA{255, 0, 0, 55},
 		true,
 	)
+
+	if g.GameOver {
+		face := &text.GoTextFace{
+			Source: MplusFaceSource,
+			Size: 48,
+		}
+		
+		text.Draw(screen, "Game Over", face, nil)		
+	}
 }
 
-//startSnakePosition: sets snake on center of the screen.
-func StartSnakePosition() []Point{
+// startSnakePosition: sets snake on center of the screen.
+func StartSnakePosition() []Point {
 	return []Point{
 		Point{
-			x: SCREEN_WIDTH/RECT_SIZE/2,
-			y:SCREEN_HEIGHT/RECT_SIZE/2,
+			x: SCREEN_WIDTH / RECT_SIZE / 2,
+			y: SCREEN_HEIGHT / RECT_SIZE / 2,
 		},
 	}
 }
 
-func (g *Game) SpawnFood(){
-	g.food = Point{x:rand.Intn(SCREEN_WIDTH/RECT_SIZE), y:rand.Intn(SCREEN_HEIGHT/RECT_SIZE)}
+func (g *Game) SpawnFood() {
+	g.food = Point{x: rand.Intn(SCREEN_WIDTH / RECT_SIZE), y: rand.Intn(SCREEN_HEIGHT / RECT_SIZE)}
 }
 
-func (g *Game) updateSnake(){
+func (g *Game) updateSnake() {
 	head := &g.Snake[0]
 
 	newHead := Point{head.x + g.Direction.x, head.y + g.Direction.y}
 
-	if newHead == g.food{
+	if newHead == g.food {
 		g.SpawnFood()
 		g.Snake = append([]Point{newHead}, g.Snake[:len(g.Snake)]...)
-	}else{
-		g.Snake = append([]Point{newHead}, g.Snake[:len(g.Snake) - 1]...)
+	} else {
+		g.Snake = append([]Point{newHead}, g.Snake[:len(g.Snake)-1]...)
 	}
 
-	
 }
 
-var(
-	dirUp = Point{x:0, y:-1}
-	dirDown = Point{x:0, y:1}
-	dirLeft = Point{x:-1, y:0}
-	dirRight = Point{x:1, y:0}
+var (
+	dirUp           = Point{x: 0, y: -1}
+	dirDown         = Point{x: 0, y: 1}
+	dirLeft         = Point{x: -1, y: 0}
+	dirRight        = Point{x: 1, y: 0}
+	MplusFaceSource *text.GoTextFaceSource
 )
 
-func (g *Game) keyboardListenerBind(){
-	if ebiten.IsKeyPressed(ebiten.KeyUp){
+func (g *Game) keyboardListenerBind() {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
 		g.Direction = dirUp
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyDown){
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
 		g.Direction = dirDown
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyLeft){
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		g.Direction = dirLeft
 	}
-	if ebiten.IsKeyPressed(ebiten.KeyRight){
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
 		g.Direction = dirRight
 	}
 }
